e1baf20a6ebbfd39baf9e250b603938548a71841
Mohamed Said <Mohamed Said@DESKTOP-HQC9UF0>
2025-02-22 19:19:37 +0200

    Add project files.

---
+++ challange1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace challange1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          // Name  
+              Console.WriteLine("Hello User");
+              Console.WriteLine("What's you'r name?");
+            string name = Console.ReadLine();
+              Console.WriteLine("Hello " + name);
+
+          // age
+              Console.WriteLine("how old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("ummm i wish you a long life");
+
+
+          // Fav Numper
+              Console.WriteLine("pleas, Inter you'r Fav. Numper betwen 1:100");
+              int theNumper = Convert.ToInt32(Console.ReadLine());
+            if (theNumper > 100 || theNumper < 1)
+            { Console.WriteLine(theNumper + " is not betwen 1:100"); }
+            else
+            {
+                Console.WriteLine($"Really! {theNumper} is my Fav numper too");
+            }
+
+
+        }
+    }
+}
---
+++ task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+using System.Linq.Expressions;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // charges
+               int small = 25;
+               int large = 35;
+               double tax = 6.6;
+
+            // list
+                  Console.WriteLine("Price per smaal Room = 25$");
+                  Console.WriteLine("Price per large Room = 35$");
+                  Console.WriteLine("Tax = 6.6$");
+                  Console.WriteLine("the estimate is valid for 30 day's");
+
+            // carpet Quantity
+                  Console.WriteLine("How Many Small Carpet you need to clean?");
+               int smallOne = Convert.ToInt32(Console.ReadLine());
+                  Console.WriteLine("How Many large Carpet you need to clean?");
+               int largeOne = Convert.ToInt32(Console.ReadLine());
+
+            // cost
+               int cost = (small * smallOne) + (large * largeOne);
+                  Console.WriteLine("Total cost = " + cost + "$");
+
+            // Tax
+                  Console.WriteLine("+Tax =" + (tax));
+                  Console.WriteLine("total estimated = " + (cost + tax) + "$");
+
+        }
+    }
+}
