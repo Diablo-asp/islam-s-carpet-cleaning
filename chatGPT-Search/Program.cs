06221c5cbc0aa7663963a818bd2d9689711ec337
Diablo-Asp <Di0ab0lo0@gmail.com>
2025-02-23 21:10:22 +0200

    ChatGpt Research

---
+++ chatGPT-Search/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace chatGPT_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // try spicifier example
+
+            int Value1 = 200;
+            int Value2 = 22;
+            double Value3 = 123.676;
+            double Value4 = 20.25;
+            int Value5 = 200;
+
+            Console.WriteLine($"{Value1} + {Value2} = {Value1 + Value2:C}");
+            Console.WriteLine($"{Value5} / {Value4} = {Value5 / Value4:P}");
+            Console.WriteLine($"{Value3} * {Value4} = {Value3 * Value4:N}");
+
+
+        }
+    }
+}
